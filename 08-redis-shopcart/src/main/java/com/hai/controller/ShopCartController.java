47931a340cf5abc264bb8e10c38100144970ed56
merry <1179193252@qq.com>
2021-07-19 21:12:22 +0800

    修改了小米购物车功能

--- 08-redis-shopcart/src/main/java/com/hai/controller/ShopCartController.java
+++ 08-redis-shopcart/src/main/java/com/hai/controller/ShopCartController.java
@@ -2,98 +2,106 @@ package com.hai.controller;
 
 import com.hai.pojo.ShopCart;
 import com.hai.utlis.ObjectUtils;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Controller
 public class ShopCartController {
 
-    private final static String USERIDKEY ="userID:";
+
+    private final static Logger log = LoggerFactory.getLogger(ShopCartController.class);
+
+    private final static String SHOPCARTID = "shopcartid:";
 
     @Autowired
-    StringRedisTemplate redisTemplate;
+    RedisTemplate redisTemplate;
 
 
-/**
- *
- * 跳转到商品首页
- * @return java.lang.String
- * @author merry
- * @creed: Talk is cheap,show me the code
- * @date 2021/7/19 18:01
- */
+    /**
+     * 跳转到商品首页
+     *
+     * @return java.lang.String
+     * @author merry
+     * @creed: Talk is cheap,show me the code
+     * @date 2021/7/19 18:01
+     */
     @GetMapping("/index")
     public String shopcart() {
         return "shopcart";
     }
 
     /**
-     *
      * 商品添加
+     *
      * @return java.lang.String
      * @author merry
      * @creed: Talk is cheap,show me the code
      * @date 2021/7/19 18:02
      */
 
+    @ResponseBody
     @PostMapping("/shopcart/add")
     public ShopCart shopcartAdd(ShopCart shopCart) throws IllegalAccessException {
- // 1: 定义每个用户的购物车的 key
-        String shopcartKey = USERIDKEY + shopCart.getUserid();
-        // 2: 获取一个操作hash的数据对象
-        HashOperations hashOperations = redisTemplate.opsForHash();
-        // 3：判断购物车是否存在
-        Boolean hasKey = hashOperations.getOperations().hasKey(shopcartKey);
-        // 如果购物车存在
-        if (hasKey) {
-            Map<String, Object> map = ObjectUtils.objectToMap(shopCart);
-            // 做功能增强：你可靠如果是相同的商品的把数量进行递增+1
-            // 5：如果不存在，就直接覆盖最新的数据进去&设置过期时间30天
-//            Object o = hashOperations.get(shopcartKey, shopCart.getProductid());
-            hashOperations.put(shopcartKey, shopCart.getProductid().toString(), map);
-            // 6: 设置购物车的过期时间
-            this.redisTemplate.expire(shopcartKey, 30, TimeUnit.DAYS);
+//        定义购物车的key
+        String shopCartid = SHOPCARTID + shopCart.getUserid();
+
+
+        Boolean aBoolean = redisTemplate.opsForHash().getOperations().hasKey(shopCartid);
+        log.info("当前的值是否存在：" + aBoolean);
+//       判断当前的购物车是否存在
+        if (aBoolean) {
+            redisTemplate.opsForHash().put(shopCartid, shopCart.getProductid().toString(), ObjectUtils.objectToMap(shopCart));
+//            存在则设置30天的过期时间
+            redisTemplate.expire(shopCartid, 30, TimeUnit.DAYS);
+
         } else {
-            // 参数1： shopcartKey 这个是redis操作的key，其实就购物车
-            // 参数2： shopCart.getProductid().toString() 这个是hash数据结构map的key,也就是商品的id
-            // 参数3： map 对应的是具体的商品
-            // 一句话：就把指定的shopCart.getProductid()添加到指定的用户的shopcartKey购物车中，具体的详细内容是map
-            // 4: 然后把shopCart商品添加到redishash中
-            Map<String, Object> map = ObjectUtils.objectToMap(shopCart);
-            hashOperations.put(shopcartKey, shopCart.getProductid().toString(), map);
+
+            redisTemplate.opsForHash().put(shopCartid, shopCart.getProductid().toString(), ObjectUtils.objectToMap(shopCart));
         }
-        // 7 :添加成功返回即可
+
+
         return shopCart;
     }
 
 
     /**
-     *
      * 商品更新
+     *
      * @return java.lang.String
      * @author merry
      * @creed: Talk is cheap,show me the code
      * @date 2021/7/19 18:02
      */
-
+    @ResponseBody
     @PostMapping("/shopcart/update")
-    public String shopcartUpdate() {
+    public String shopcartUpdate(ShopCart shopCart) throws IllegalAccessException {
+        String shopCartid = SHOPCARTID + shopCart.getUserid();
+        Boolean aBoolean = redisTemplate.opsForHash().getOperations().hasKey(shopCartid);
+        redisTemplate.opsForHash().put(shopCartid, shopCart.getProductid().toString(), ObjectUtils.objectToMap(shopCart));
+//            存在则设置30天的过期时间
+        redisTemplate.expire(shopCartid, 30, TimeUnit.DAYS);
         return "shopcart";
     }
 
 
-
     /**
-     *
      * 商品删除
+     *
      * @return java.lang.String
      * @author merry
      * @creed: Talk is cheap,show me the code
@@ -101,25 +109,58 @@ public class ShopCartController {
      */
 
     @PostMapping("/shopcart/del")
-    public String shopcartDel() {
-        return "shopcart";
+    public String shopcartDel(Long userid, Long productid) {
+        String shopCartid = SHOPCARTID + userid;
+       redisTemplate.opsForHash().delete(shopCartid, productid.toString());
+        return "suscc";
     }
 
 
-
     /**
-     *
      * 商品查询
+     *
      * @return java.lang.String
      * @author merry
      * @creed: Talk is cheap,show me the code
      * @date 2021/7/19 18:02
      */
 
+    @ResponseBody
     @PostMapping("/shopcart/list")
-    public String shopcartList() {
-        return "shopcart";
+    public Map<String,Object> shopcartList(Long userid) {
+        String shopCartid = SHOPCARTID + userid;
+
+//       定义总金额
+        double totalmoney = 0d;
+//   当前购物车数量的值
+        Long size = redisTemplate.opsForHash().size(shopCartid);
+//        拿到k v键值对
+        Map<String, Map<String, Object>> entries = redisTemplate.opsForHash().entries(shopCartid);
+//        存放当前购物车的数据
+        List<Map<String,Object>> resultList = new ArrayList<>();
+        for (Map.Entry<String, Map<String, Object>> stringMapEntry : entries.entrySet()) {
+//            具体商品的数据
+            resultList.add(stringMapEntry.getValue());
+//            拿到当前商品的数量和金额进行运算
+            totalmoney += Integer.parseInt(String.valueOf(stringMapEntry.getValue().get("productnum"))) *
+                    Double.parseDouble(String.valueOf(stringMapEntry.getValue().get("productprice")));
+        }
+
+
+        // 6 : 返回购物车的数据信息
+        Map<String,Object> map = new HashMap<>();
+        // 总计
+        map.put("totalPrice",totalmoney);
+        // 这个商品的个数
+        map.put("totalCount",size);
+        // 具体商品的数据
+        map.put("resultList",resultList);
+
+        return map;
     }
+
+
+
 
 
 
